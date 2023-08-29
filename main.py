f4e12e3c100a37c6ca8ff52cc5363da0d5c5fef0
adafantom <61115044+adafantom@users.noreply.github.com>
2023-08-30 01:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from gtts import gTTS
 
 
 def get_pdf_file(file_path):
-  with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
-    pdf_pages = pdf_file.pages
-    pages_pdf_text = ''.join([pdf_page.extract_text() for pdf_page in pdf_pages])
-    solid_pdf_text = pages_pdf_text.replace('\n', ' ')
-    return solid_pdf_text
+    with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        pdf_pages = pdf_file.pages
+        pages_pdf_text = ''.join([pdf_page.extract_text() for pdf_page in pdf_pages])
+        solid_pdf_text = pages_pdf_text.replace('\n', ' ')
+        return solid_pdf_text
 
 
 def get_audio_file(pdf_file):
@@ -22,4 +22,4 @@ def main():
     
 
 if __name__ == '__main__':
-		main()
+    main()
